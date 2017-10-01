70e302a9c74a290481a696d18ca41a6072fb3664
Nasir Shadravan <nshadravan@gmail.com>
2017-10-01 23:51:19 +0200

    Small update on about page

--- src/containers/about.js
+++ src/containers/about.js
@@ -1,8 +1,12 @@
-import React from 'react'
-
+import React from 'react';
+import {Container} from 'reactstrap';
 export default () => (
-    <div>
-      <h1>About Us</h1>
-      <p>Hello Medium!</p>
-    </div>
+    <Container>
+      <h1>About</h1>
+      <p>Portfo is a cryptocurrency portfolio tracker on top of <a href="#">Blockstack</a> platform.
+        By using portfo, all your data will be encrypted and stored on a storage of your choosing.
+      </p>
+      <p>If you have any suggestions or want to report an issue or say hi just hit me up on <a
+          href="https://twitter.com/n4cr">twitter</a>. </p>
+    </Container>
 )

262148e10c23992a576e5c0608fccdf7fb5a28c0
Nasir Shadravan <nshadravan@gmail.com>
2017-10-06 12:44:36 +0200

    Add currency symbols

---
+++ src/utils.js
@@ -0,0 +1,39 @@
+/**
+ * Created by nasir on 06/10/2017.
+ */
+
+export const currencySymbols = {
+  "USD": "$",
+  "BTC": "Ƀ",
+  "AUD": "A$",
+  "BRL": "R$",
+  "CAD": "C$",
+  "CHF": "CHF",
+  "CLP": "CLP",
+  "CNY": "¥",
+  "CZK": "Kč",
+  "DKK": "kr",
+  "EUR": "€",
+  "GBP": "£",
+  "HKD": "HKD",
+  "HUF": "Ft",
+  "IDR": "Rp",
+  "ILS": "₪",
+  "INR": "INR",
+  "JPY": "¥",
+  "KRW": "₩",
+  "MXN": "MXN",
+  "MYR": "RM",
+  "NOK": "kr",
+  "NZD": "NZD",
+  "PHP": "₱",
+  "PKR": "₨",
+  "PLN": "zł",
+  "RUB": "₽",
+  "SEK": "kr",
+  "SGD": "SGD",
+  "THB": "฿",
+  "TRY": "TRY",
+  "TWD": "NT$",
+  "ZAR": "R"
+}

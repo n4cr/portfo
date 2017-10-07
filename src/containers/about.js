4ffa48c5e060584c7286609c3a403fcfb114ff92
Nasir Shadravan <nshadravan@gmail.com>
2017-10-07 17:58:41 +0200

    Fix link to blockstack website

--- src/containers/about.js
+++ src/containers/about.js
@@ -10,7 +10,8 @@ export default () => (
       </Row>
       <Row className="mt-3">
         <h3>About</h3>
-        <p>Portfo is a cryptocurrency portfolio tracker on top of <a href="#">Blockstack</a> platform.
+        <p>Portfo is a cryptocurrency portfolio tracker on top
+          of <a target="_blank"    href="https://blockstack.org">Blockstack</a> platform.
           By using portfo, all your data will be encrypted and stored on a storage of your choosing.
         </p>
         <p>If you have any suggestions or want to report an issue or say hi just hit me up on <a
